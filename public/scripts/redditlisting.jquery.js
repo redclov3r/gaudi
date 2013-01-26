94b5e29f0a398c5dc75a63ff5d9c9d283ea5676d
Philipp Kreutzer <philipp@3digitalarts.de>
2013-01-26 18:09:46 +0100

    using localStorage to tracked visited links

--- public/scripts/redditlisting.jquery.js
+++ public/scripts/redditlisting.jquery.js
@@ -16,6 +16,16 @@
                     //onItemActivate: function
                 }, options);
 
+                // init storage
+                var visited = []; 
+                if(Modernizr.localstorage) {
+                    if(localStorage.visited) {
+                        visited = JSON.parse(localStorage.visited); 
+                    } else {
+                        localStorage.visited = [];
+                    }
+                }
+
                 switch(settings.type) {
                     case 'search':
                         redditQuery = new RedditSearch(settings.value);
@@ -30,10 +40,29 @@
                 $this.data('redditListing', {
                     settings: settings,
                     redditQuery: redditQuery,
-                    listingTemplate: listingTemplate
+                    listingTemplate: listingTemplate,
+                    visited: visited
                 });
 
                 $this.redditListing('load');
+            });
+        },
+
+        markVisited: function(id) {
+            return this.each(function(){
+                var $this = $(this);
+                var data = $this.data('redditListing');
+
+                var $li = $this.find("[data-name=" + id + "]");
+                $li.addClass('visited');
+
+                // save new visited links
+                if(data.visited.indexOf(id) == -1) {
+                    data.visited.push(id);
+                } 
+                if(Modernizr.localstorage) {
+                    localStorage.visited = JSON.stringify(data.visited);
+                }
             });
         },
 
@@ -46,11 +75,17 @@
 
                 data.redditQuery.load(function(listingJSON) {
                     $this.redditListing('renderJSON', listingJSON.data);
-                });
 
-                if (data.settings.afterLoad) {
-                    data.settings.afterLoad(data.settings.type, data.settings.value);
-                };
+                    if (data.settings.afterLoad) {
+                        data.settings.afterLoad(data.settings.type, data.settings.value);
+                    }
+
+                    $this.find('li').each(function() {
+                        if (data.visited.indexOf($(this).data('name')) != -1) {
+                            $(this).addClass('visited');
+                        }
+                    });
+                });
             });
         },
 
@@ -66,11 +101,19 @@
                 data.redditQuery.load(function(listingJSON) {
                     $this.redditListing('renderJSON', listingJSON.data);
                     $this.redditListing('next');
+
+                    if (data.settings.afterLoad) {
+                        data.settings.afterLoad(data.settings.type, data.settings.value);
+                    }
+
+                    $this.find('li').each(function() {
+                        if (data.visited.indexOf($(this).data('name')) != -1) {
+                            $(this).addClass('visited');
+                        }
+                    });
+
                 }, lastItem);
 
-                if (data.settings.afterLoad) {
-                    data.settings.afterLoad(data.settings.type, data.settings.value);
-                };
             });
         },
 
@@ -89,6 +132,8 @@
                 if (data.settings.onItemActivate) {
                     data.settings.onItemActivate($li);
                 };
+                
+                $this.redditListing('markVisited', $li.data('name'));
             });
         },
 
