d097c1ab07b3cb47085b4c6c8bfd9ad9819b7a31
Philipp Kreutzer <philipp@3digitalarts.de>
2013-01-29 12:33:17 +0100

    transformed reddit listing to Backbone View and moved it into main.js. Started event delegation using backbone

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -44,6 +44,11 @@ var spinnerOpts = {
 
 
 var Browser = Backbone.View.extend({
+    events: {
+        "mouseenter" : "show",
+        "mouseleave" : "hide"
+    },
+
     setLoading: function(state) {
         if(state) {
             this.spinner = new Spinner(spinnerOpts).spin(this.el);
@@ -90,9 +95,195 @@ var Browser = Backbone.View.extend({
 
     openLink: function($a) {
         this.load($a.attr('href'));
+    },
+
+    show: function() {
+        if (this.$el.children().length > 0)
+            this.$el.addClass('active');
+    },
+
+    hide: function() {
+        this.$el.removeClass('active');
     }
 });
 
+
+var redditListingView = Backbone.View.extend({
+    events: {
+        "click .listing__item__link": function(e) {
+            e.preventDefault();
+            this.activateItem($(e.target).closest('li'));
+        }
+    },
+
+    options: {
+        type: "sr",
+        value: "pics"
+    },
+
+    initialize: function() {
+        // initialize storage
+        this.visited = []; 
+        if(Modernizr.localstorage) {
+            if(localStorage.visited) {
+                this.visited = JSON.parse(localStorage.visited); 
+
+                // trim
+                if (this.visited.length > 400) {
+                    this.visited = visited.slice(100);
+                }
+            } else {
+                localStorage.visited = [];
+            }
+        }
+
+        // create Query object
+        switch(this.options.type) {
+            case 'search':
+                this.redditQuery = new RedditSearch(this.options.value);
+                break;
+            case 'sr':
+                this.redditQuery = new RedditSubreddit(this.options.value);
+                break;
+        }
+
+        // compile Template
+        this.listingTemplate = Handlebars.compile($('#listing-template').html());
+
+        this.load();
+    },
+
+
+    markVisited: function(id) {
+        var $li = this.$("[data-name=" + id + "]");
+        $li.addClass('visited');
+
+        // save new visited links
+        if(this.visited.indexOf(id) == -1) {
+            this.visited.push(id);
+        } 
+        if(Modernizr.localstorage) {
+            localStorage.visited = JSON.stringify(this.visited);
+        }
+    },
+
+
+    load: function() {
+        var me = this;
+
+        this.setLoading(true);
+
+        this.redditQuery.load(function(listingJSON) {
+            me.renderJSON(listingJSON.data);
+
+            if (me.options.afterLoad) {
+                me.options.afterLoad(me.options.type, me.options.value);
+            }
+
+            // mark all items present in visited-storage
+            me.$('li').each(function() {
+                if (me.visited.indexOf($(this).data('name')) != -1) {
+                    $(this).addClass('visited');
+                }
+            });
+        });
+    },
+
+
+    loadMore: function() {
+        var me = this;
+        this.setLoading(true);
+
+        var lastItem = this.$('li').last().data('name');
+
+        data.redditQuery.load(function(listingJSON) {
+            me.renderJSON(listingJSON.data);
+            me.next();
+
+            if (me.options.afterLoad) {
+                me.options.afterLoad(me.options.type, me.options.value);
+            }
+
+            // mark all items present in visited-storage
+            me.$('li').each(function() {
+                if (me.visited.indexOf($(this).data('name')) != -1) {
+                    $(this).addClass('visited');
+                }
+            });
+
+        }, lastItem);
+    },
+
+
+    activateItem: function($li) {
+        var me = this;
+
+        $li.siblings().removeClass('active');
+        $li.addClass('active');
+
+        // scroll to active
+        $('html, body').stop(true).animate({scrollTop : ($li.offset().top - 47 - 36)}, 'slow');
+
+        if (me.options.onItemActivate) {
+            me.options.onItemActivate($li);
+        }
+
+        this.markVisited($li.data('name'));
+    },
+
+    renderJSON: function(json) {
+        this.setLoading(false);
+
+        var listingHTML = this.listingTemplate(json);
+        this.$el.append(listingHTML);
+    },
+
+
+    setLoading: function(state) {
+        if(this.spinner !== undefined && this.spinner.stop !== undefined) {
+            this.spinner.stop();
+        }
+        $('#spinner').hide().removeClass('active');
+
+        if(state) {
+            $('#spinner').show().addClass('active');
+            this.spinner = new Spinner(spinnerOpts).spin(document.getElementById('spinner'));
+        }
+    },
+
+
+    next: function() {
+        var $activeli = this.$('li.active');
+        var $nextli;
+
+        if($activeli.length === 0) {
+            $nextli = this.$('li').first();
+        } else {
+            $nextli = $activeli.next();
+        }
+        if ($nextli.length > 0) {
+            this.activateItem($nextli);
+        } else {
+            this.loadMore();
+        }
+    },
+
+
+    prev: function() {
+        var $activeli = this.$('li.active');
+        var $nextli;
+
+        if($activeli.length === 0) {
+            $nextli = this.$('li').last();
+        } else {
+            $nextli = $activeli.prev();
+        }
+
+        if ($nextli.length > 0) {
+            this.activateItem($nextli);
+        } 
+    }
+});
 
 
 $(function() {
@@ -122,6 +313,7 @@ $(function() {
         }
     }
     var $listing;
+    var listing;
 
     var trackView = function(path) {
         if (_gaq !== null) {
@@ -133,17 +325,27 @@ $(function() {
     }
 
     var openSubreddit = function(sr) {
+        /*
         $listing = $('<ul class="listing">')
             .redditListing($.extend({type:"sr", value:sr}, defaultRedditSettings))
             .appendTo($('#results').empty());
+            */
+
+        var $ul = $('<ul class="listing">').appendTo($('#results').empty());
+        listing = new redditListingView($.extend({ el: $ul, type:"sr", value:sr }, defaultRedditSettings));
+        console.log(listing);
 
         $('#browser').removeClass('active');
     }
 
     var openSearch = function(q) {
+        /*
         $listing = $('<ul class="listing">')
             .redditListing($.extend({type:"search", value:q}, defaultRedditSettings))
             .appendTo($('#results').empty());
+            */
+        var $ul = $('<ul class="listing">').appendTo($('#results').empty());
+        listing = new redditListingView($.extend({ el: $ul, type:"search", value:q }, defaultRedditSettings));
 
         $('#browser').removeClass('active');
     }
@@ -173,44 +375,30 @@ $(function() {
         openSubreddit(sr);
     });
 
-    $(document).on('mouseenter', '#browser', function(e){
-        if ($('#browser').children().length > 0)
-            $('#browser').addClass('active');
-    });
-
-    $(document).on('mouseleave', '#browser', function(e){
-        $('#browser').removeClass('active');
-    });
-
     $(document).on('click', 'a.internal', function(e){
         e.preventDefault();
         browser.openLink($(this));
     });
 
-    $(document).on('click', '.listing__item__link', function(e){
-        e.preventDefault();
-        $listing.redditListing('activateItem', $(this).closest('li'));
-    });
-
     $(document).on('keydown', function(e){
         switch(e.which) {
             case 40:
-                $listing.redditListing('next');
+                listing.next();
                 e.preventDefault();
                 break;
             
             case 38:
-                $listing.redditListing('prev');
+                listing.prev();
                 e.preventDefault();
                 break;
 
             case 39:
-                $('#browser').removeClass('active');
+                browser.hide();
                 e.preventDefault();
                 break;
             
             case 37:
-                $('#browser').addClass('active');
+                browser.show();
                 e.preventDefault();
                 break;
             
